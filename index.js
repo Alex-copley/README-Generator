db3fed24c9ae1f61b05cf2d819f9ad7908d4c095
Timothy Copley <alexmcguire2000@gmail.com>
2021-10-21 20:22:12 -0400

    Added basic License data for markdown and added necessary questions to prompt. Need to add logic behind prompting questions and then to put them into a readme.md file

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
 // TODO: Include packages needed for this application
+const { prompt } = require("inquirer")
+const { writeFile } = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username.",
+        default: "Alex-copley"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address.",
+        default: "alexmcguire2000@gmail.com"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Title this project.",
+        default: "NULL"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the intended purpose of this project.",
+        default: "NULL"
+    },
+    {
+        type: "input",
+        name: "install",
+        message:
+            "What dependencies are needed to be installed to run this application?",
+        default: "NULL"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message:
+            "Provide steps on how to use the application once the user has installed it.",
+        default: "Run in terminal"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What is the command used to run tests on this application?",
+        default: "node index.js"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message:
+            'Provide the names of anyone who has contributed to this project, or write "Sole Author."',
+        default: "Sole Author"
+    },
+    {
+        type: "list",
+        name: "license",
+        message:
+            "Which of the following best describes your situation? We will pick a license that is right for you.",
+        choices: ["ISC", "MIT", "GNU GPLv3"],
+        default: "MIT"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,100 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const input = license;
+  switch(input) {
+    case 'ISC':
+      response = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]";
+      return response;
+    case 'MIT':
+      response = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+      return response;
+    case 'BSD 3':
+      response = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+      return response;
+    case 'GPL 3.0':
+      response = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+      return response;
+    case 'Apache 2.0':
+      response = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+      return response;
+    case 'None':
+      response = "";
+      return response;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const input = license;
+  switch(input) {
+    case 'ISC':
+      response = "(https://opensource.org/licenses/ISC)";
+      return response;
+    case 'MIT':
+      response = "(https://opensource.org/licenses/MIT)";
+      return response;
+    case 'BSD 3':
+      response = "(https://opensource.org/licenses/BSD-3-Clause)";
+      return response;
+    case 'GPL 3.0':
+      response = "(https://www.gnu.org/licenses/gpl-3.0)";
+      return response;
+    case 'Apache 2.0':
+      response = "(https://opensource.org/licenses/Apache-2.0)";
+      return response;
+    case 'None':
+      response = "";
+      return response;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "") {
+    return "";
+  } else {
+    return `## License
+  This project is covered under the ${license} license. Visit the following link for more information on this license: [${license}]${renderLicenseLink(license)}`;
+  };
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseSection(data.license)}  
 
-`;
+# Description  
+${data.description}  
+
+# Table of contents  
+[Installation](#installation)  
+[Usage](#usage)  
+[License](#license)  
+[Contributors](#contributors)  
+[Tests](#tests)  
+[Questions](#questions)  
+
+# Installation 
+The following dependencies must be installed to use this application: ${data.install}  
+
+# Usage  
+How to use the application: ${data.usage}  
+
+# License  
+This project is licensed under the ${data.license} license.  
+
+# Contributors  
+${data.contributors}  
+
+# Tests
+Command used to run tests on this application: ${data.tests}  
+
+# Questions  
+If you have any questions, please contact me at ${data.email}
+Github Link: https://github.com/${data.github} `;
 }
 
 module.exports = generateMarkdown;
